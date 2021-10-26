39c9eff6ce77ba36cef9810488f492fac4e1f622
NatesServices <93129005+NatesServices@users.noreply.github.com>
2021-10-26 17:39:57 -0400

    Add files via upload

---
+++ announcer/config.lua
@@ -0,0 +1,10 @@
+Basic = {
+    AnnouncerDelay = 10, -- in seconds must be integer
+    Msgs = { -- You can add unlimited messages
+        [1] = {text = "Message 1 Here"},
+        [2] = {text = "Message 2 Here"},
+        [3] = {text = "Message 3 Here"},
+        [4] = {text = "Message 4 Here"},
+    }
+}
+
